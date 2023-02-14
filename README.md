3d81004894f75438d699d8deb7e73a288805fa41
Min-ki Park <browneyedsoul@yonsei.ac.kr>
2023-02-15 03:11:29 +0900

    3rd commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # RemNote Active Recall
 
-<!-- ![head](https://browneyedsoul.click/_next/image?url=%2Fprojects%2Fremnote-css-library%2Fhead.png&w=3840&q=75) -->
+## Usage
 
+This plugin converts all the clozes and list answers into hidden state.
+When you hover mouse or focus the cursor on them, they will be visible.
+
+## Another way
+
+If you'd like to apply this especially within a tagged rem, you can refer this @import string.
+Just copy it and paste to your Custom CSS Codeblock.
+
+```css
+@import url("https://browneyedsoul.github.io/RemNote-CSS-Library/Active%20Recall.css");
+```
+
+And then, apply the `Active Recall` Power-up Tag in a rem where you'd like to apply hidden state.
--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -1,6 +1,46 @@
-import { declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
+import { useTracker, declareIndexPlugin, ReactRNPlugin } from "@remnote/plugin-sdk";
 
-async function onActivate(plugin: ReactRNPlugin) {}
+export const ACTIVE_RECALL = "active-recall_power-up";
+
+let ARCSS: string;
+let ARTAGGED: string;
+
+async function onActivate(plugin: ReactRNPlugin) {
+  await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-ActiveRecall/main/src/snippet.css")
+    .then((response) => response.text())
+    .then((text) => {
+      ARCSS = text;
+      console.dir("Active Recall Installed");
+    })
+    .catch((error) => console.error(error));
+  await fetch("https://raw.githubusercontent.com/browneyedsoul/RemNote-ActiveRecall/main/src/untag.css")
+    .then((response) => response.text())
+    .then((text) => {
+      ARTAGGED = text;
+      console.dir("Active Recall UnTagged Installed");
+    })
+    .catch((error) => console.error(error));
+  await plugin.app.registerPowerup("Active Recall", ACTIVE_RECALL, "A Power-up for covering cloze", { slots: [] });
+  await plugin.app.registerCommand({
+    id: "active-recall",
+    name: "Active Recall",
+    action: async () => {
+      const rem = await plugin.focus.getFocusedRem();
+      await rem?.addPowerup(ACTIVE_RECALL);
+    },
+  });
+  await plugin.settings.registerBooleanSetting({
+    id: "scope",
+    title: "Scope Expander",
+    defaultValue: false,
+  });
+  await useTracker(async (reactivePlugin) => {
+    const arSetting = await reactivePlugin.settings.getSetting<boolean>("scope");
+    arSetting
+      ? await plugin.app.registerCSS("active-recall-untagged", ARCSS)
+      : await plugin.app.registerCSS("active-recall", ARCSS);
+  });
+}
 
 async function onDeactivate(_: ReactRNPlugin) {}
 
